216e7a7ac8066866aaec7b0efc00a51170121ee2
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-05-25 10:12:55 +0600

    car-update

--- src/app/(withDashboardLayout)/dashboard/admin/adminAction/action.ts
+++ src/app/(withDashboardLayout)/dashboard/admin/adminAction/action.ts
@@ -32,58 +32,58 @@ import { cookies } from "next/headers";
 //     throw error;
 //   }
 // }
-// export async function updateCar(
-//   carId: string,
-//   pre: FormData,
-//   formData: FormData
-// ) {
-//   try {
-//     const newFormData = {
-//       ...Object?.fromEntries(formData),
-//       rating: Number(Object?.fromEntries(formData).rating),
-//       passengerCapacity: Number(
-//         Object?.fromEntries(formData).passengerCapacity
-//       ),
-//     };
+export async function updateCar(
+  carId: string,
+  pre: FormData,
+  formData: FormData
+) {
+  try {
+    const newFormData = {
+      ...Object?.fromEntries(formData),
+      rating: Number(Object?.fromEntries(formData).rating),
+      passengerCapacity: Number(
+        Object?.fromEntries(formData).passengerCapacity
+      ),
+    };
 
-//     const formattedData = JSON.stringify(newFormData);
+    const formattedData = JSON.stringify(newFormData);
 
-//     const accessToken = cookies().get("accessToken")?.value;
-//     const headers = new Headers();
-//     headers.append("Authorization", accessToken!);
-//     headers.append("Content-Type", "application/json");
+    const accessToken = cookies().get("accessToken")?.value;
+    const headers = new Headers();
+    headers.append("Authorization", accessToken!);
+    headers.append("Content-Type", "application/json");
 
-//     const res = await fetch(`${process.env.serverUrl}/cars/${carId}`, {
-//       method: "PATCH",
+    const res = await fetch(`${process.env.localUrl}/cars/${carId}`, {
+      method: "PATCH",
 
-//       headers: headers,
-//       body: formattedData,
-//     });
-//     revalidateTag("cars");
-//     const data = await res.json();
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-// export async function deleteCar(carId: string) {
-//   try {
-//     const accessToken = cookies().get("accessToken")?.value;
-//     const headers = new Headers();
-//     headers.append("Authorization", accessToken!);
-//     headers.append("Content-Type", "application/json");
+      headers: headers,
+      body: formattedData,
+    });
+    revalidateTag("cars");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+export async function deleteCar(carId: string) {
+  try {
+    const accessToken = cookies().get("accessToken")?.value;
+    const headers = new Headers();
+    headers.append("Authorization", accessToken!);
+    headers.append("Content-Type", "application/json");
 
-//     const res = await fetch(`${process.env.serverUrl}/cars/${carId}`, {
-//       method: "DELETE",
-//       headers: headers,
-//     });
-//     revalidateTag("cars");
-//     const data = await res.json();
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+    const res = await fetch(`${process.env.serverUrl}/cars/${carId}`, {
+      method: "DELETE",
+      headers: headers,
+    });
+    revalidateTag("cars");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
 
 export async function creteACar(pre: FormData, formData: FormData) {
   try {
